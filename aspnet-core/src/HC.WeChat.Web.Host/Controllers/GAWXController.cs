3a514ed80b1fdabb309f4c34fa0c4cebd95b76ff
DonaldTdz <tangdezou@qq.com>
2018-07-21 00:08:59 +0800

    陈列活动修改

--- aspnet-core/src/HC.WeChat.Web.Host/Controllers/GAWXController.cs
+++ aspnet-core/src/HC.WeChat.Web.Host/Controllers/GAWXController.cs
@@ -19,9 +19,9 @@ namespace HC.WeChat.Web.Host.Controllers
         IShopAppService _shopAppService;
         //private readonly IConfigurationRoot _appConfiguration;
         //private WeChatTenantSetting _settings;
-        //private string host = "http://ga.intcov.com";
+        private string host = "http://ga.intcov.com";
         //private string host = "http://localhost:21021";
-        private string host = "http://wx.photostory.top";
+        //private string host = "http://wx.photostory.top";
         private int? tenantId;
 
         private string UserOpenId
@@ -94,7 +94,7 @@ namespace HC.WeChat.Web.Host.Controllers
             //UserOpenId = "oPM5Uv81jfyJqWbVxWAH-RUqsCAs";
             //UserOpenId = "oPM5Uv89yy7Iv8k9gLHjjsMTT5Gw";//零售户
             //UserOpenId = "oB4nYjnoHhuWrPVi2pYLuPjnCaU0"; //杨帆专用
-            UserOpenId = "oWusewEMAQ_9km_ME19diwMrEop4"; // 199
+            //UserOpenId = "oWusewEMAQ_9km_ME19diwMrEop4"; // 199
             //UserOpenId = "oPM5Uv89yy7Iv8k9gLHjjsMTT5Gw";
             //UserOpenId = "oB4nYjnoHhuWrPVi2pYLuPjnCaU1"; //杨帆专用
             //UserOpenId = "oWusewPRxWuP4wMz3UmHR0y7CJME"; //回家测试用
