de9555331ba1958257380b7a03a7abe70423b798
shiguanWXX <1358110409@qq.com>
2018-07-27 10:50:23 +0800

    job是否启动配置

--- aspnet-core/src/HC.WeChat.Application/LevelJob/UpdateLevelWorker.cs
+++ aspnet-core/src/HC.WeChat.Application/LevelJob/UpdateLevelWorker.cs
@@ -15,6 +15,7 @@ using Abp.AutoMapper;
 using Abp.Dependency;
 using Abp.Domain.Uow;
 using HC.WeChat.Products;
+using HC.WeChat.MemberConfigs;
 
 namespace HC.WeChat.LevelJob
 {
@@ -22,33 +23,38 @@ namespace HC.WeChat.LevelJob
     {
         private readonly IProductAppService _productAppService;
         private DateTime preDate = DateTime.Now.AddDays(-1);//用于控制在合适时间段中只执行一次档级更新(保证只会去数据库去请求一次levellog的存在)
-
+        private readonly IMemberConfigAppService _memberConfigAppService;
         public UpdateLevelWorker(AbpTimer timer,
-          IProductAppService productAppService) : base(timer)
+          IProductAppService productAppService, IMemberConfigAppService memberConfigAppService) : base(timer)
         {
-            Timer.Period = 3600000;
-            //Timer.Period = 10000;
+            //Timer.Period = 3600000;
+            Timer.Period = 10000;
             _productAppService = productAppService;
             //启动日志
             //Logger.InfoFormat("启动job时间：{0}", DateTime.Now);
             //DoWork();
+            _memberConfigAppService = memberConfigAppService;
         }
         [UnitOfWork]
         protected override void DoWork()
         {
             Logger.InfoFormat("进入job开始时间：{0}", DateTime.Now);
-            var s = DateTime.Today;
-            var m = DateTime.Now.AddDays(-1);
-            if (DateTime.Now.Day == 3 && DateTime.Now.Hour >= 4 && preDate != DateTime.Today)
+            var jobConfig = _memberConfigAppService.GetJobConfig();
+            if (jobConfig.Value == "true")
             {
-                Logger.InfoFormat("执行job逻辑开始时间：{0}", DateTime.Now);
-                preDate = DateTime.Today;
-                using (CurrentUnitOfWork.DisableFilter(AbpDataFilters.MayHaveTenant))
+                var s = DateTime.Today;
+                var m = DateTime.Now.AddDays(-1);
+                if (DateTime.Now.Day == 3 && DateTime.Now.Hour >= 4 && preDate != DateTime.Today)
                 {
-                    _productAppService.UpdateLevel();
-                    CurrentUnitOfWork.SaveChanges();
+                    Logger.InfoFormat("执行job逻辑开始时间：{0}", DateTime.Now);
+                    preDate = DateTime.Today;
+                    using (CurrentUnitOfWork.DisableFilter(AbpDataFilters.MayHaveTenant))
+                    {
+                        _productAppService.UpdateLevel();
+                        CurrentUnitOfWork.SaveChanges();
+                    }
+                    Logger.InfoFormat("执行job逻辑结束时间：{0}", DateTime.Now);
                 }
-                Logger.InfoFormat("执行job逻辑结束时间：{0}", DateTime.Now);
             }
         }
     }
