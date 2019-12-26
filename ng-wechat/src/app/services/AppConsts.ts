75a2d16d14bd32f6f6a7b766ba513ce1aa312638
YangFan17 <173838362@qq.com>
2019-12-26 10:00:53 +0800

    抽奖效果DEMO

--- ng-wechat/src/app/services/AppConsts.ts
+++ ng-wechat/src/app/services/AppConsts.ts
@@ -3,5 +3,6 @@ export class AppConsts {
     static remoteServiceBaseUrl: string = 'http://localhost:21021';
     // static remoteServiceBaseUrl: string = 'http://hcwx.sayequ.me';
     // static remoteServiceBaseUrl: string = 'http://ga.hechuangcd.com';
+    // static remoteServiceBaseUrl: string = 'http://gy.hechuangcd.com';
     static appBaseUrl: string;
 }
